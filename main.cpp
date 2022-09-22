ac4ffdf2bd825fed4f0cb18cca0391573f3a6d04
Jrsanch6 <99140779+Jrsanch6@users.noreply.github.com>
2022-09-22 13:10:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 
 int main() {
 
-  BMP bmpNew(769, 769, false);
+  BMP bmpNew(800, 800, false);
   bmpNew.fill_region(0, 0, 200, 200, 0, 0, 0, 0);
-  for (int i = 0; i < bmpNew.bmp_info_header.width; i++) {
-    int y = ((sqrt((768 + i) * (768 - i))) / 2) + 384;
-    int yBottom = -((sqrt((768 + i) * (768 - i))) / 2) + 384;
-    bmpNew.set_pixel(i, y, 255, 255, 255, 0);
+  for (float i = 0; i < bmpNew.bmp_info_header.width; i = i + 0.001){
+    float y = ((sqrt((768 + i) * (768 - i))) / 2) + 384;
+    float yBottom = -((sqrt((768 + i) * (768 - i))) / 2) + 384;
+    bmpNew.set_pixel(i, y, 255, 255, 255,0);
     bmpNew.set_pixel(i, yBottom, 255, 255, 255, 0);
   }
   bmpNew.write("output.bmp");
