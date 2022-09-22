afbe6c8ce920b200e34de218ed8fe89c28ab85b7
Jrsanch6 <99140779+Jrsanch6@users.noreply.github.com>
2022-09-22 13:15:32 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
 
   BMP bmpNew(800, 800, false);
   bmpNew.fill_region(0, 0, 200, 200, 0, 0, 0, 0);
-  for (float i = 0; i < bmpNew.bmp_info_header.width; i = i + 0.001){
+  for (float i = 0; i < 768; i = i + 0.001){
     float y = ((sqrt((768 + i) * (768 - i))) / 2) + 384;
     float yBottom = -((sqrt((768 + i) * (768 - i))) / 2) + 384;
     bmpNew.set_pixel(i, y, 255, 255, 255,0);
